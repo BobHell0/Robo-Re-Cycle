1b83f8ed19ba57ee625e4090b5517ce6d2cce8b2
anujan-c <161555579+anujan-c@users.noreply.github.com>
2024-10-09 12:14:37 +1100

    annotated predictStillImages

--- computerVision/computerVisionModel__pythonFiles/predictStillImages.py
+++ computerVision/computerVisionModel__pythonFiles/predictStillImages.py
@@ -2,21 +2,21 @@ from ultralytics import YOLO
 from writeCoordsToFile import writeCoords, clearCoordsFile
 from pixelToMillimetreConversion import convertPxToMm
 
-ORIGIN_POINT = (266.7, 71.0)
+ORIGIN_POINT = (266.7, 71.0)    # How this was achieved can be read about in findingOrigin.md
 if __name__ == "__main__":
 
-    # ORIGIN_POINT_MM = 
-
-    # Load a model
-    modelFile = "/Users/unswaccount/Documents/Studying/Year 2/VIP - Robo-Re-Cycle/Object Detection Data/pythonFiles.py/runs/detect/screwsAndHoles_5/weights/best.pt"
-    model = YOLO(f"{modelFile}")  # pretrained YOLOv8n model
+    modelFile = "example.pt"        # This is the .pt file you get AFTER training an computer vision model
+    model = YOLO(f"{modelFile}")    # Lets us load up the already trained YOLOv8n model
 
     # Run batched inference on a list of images
-    imageFiles = "/Users/unswaccount/Desktop/VIP Wk 4 Thursday/LinedPaper__30_35_37_37/test1.jpg"
+    imageFiles = "exampleImages"        # can either be a path to one image or a whole directory with images
 
-    model.predict(f"{imageFiles}", save=True, imgsz=320, conf=0.05, show_boxes=True, iou=0.1)
+    # capturing the results in a variable called "results"
+    #   save=True saves the images WITH bounding boxes in the same direcotry
+    #   conf=0.01 lets you set the THRESHOLD CONFIDENCE (to 1% in this case)
+    #   iou refers to likelyhood of overlap – 0.1 means there shouldn't be overlapping bounding boxes
 
-    results = model.predict(f"{imageFiles}", save=False, imgsz=320, conf=0.01)
+    results = model.predict(f"{imageFiles}", save=True, imgsz=320, conf=0.01, iou=0.1)
 
     # Extract bounding boxes, classes, names, and confidences
     boxes = results[0].boxes.xyxy.tolist()
@@ -31,17 +31,17 @@ if __name__ == "__main__":
         confidence = conf
         detected_class = cls
         name = names[int(cls)]
-        # print(f"Found a {cls}")
-        # print(f"Top Left coords = {x1}, {y1} | Bottom Right coords = {x2}, {y2}")
+
         centreX = (x1 + x2) / 2
         centreY = (y1 + y2) / 2
         
-        # print(f"centreX = {centreX}; centreY = {centreY} in PIXELS")
-
+        # This conversion process requires the PIXEL_TO_MM ratio. How this ratio
+        # was determined can be read about in findingPxToMMRatio.md
         converted_CentreX = convertPxToMm(centreX)
         converted_CentreY = convertPxToMm(centreY)
 
 
-        
+        # Writes the finalised, global coordinates to a file for the Roland to use
+        # to approach a screw    
         writeCoords(ORIGIN_POINT[0] - converted_CentreX, ORIGIN_POINT[1] + converted_CentreY )
         
