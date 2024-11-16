9da4e66fdc18ad196b89133155469fcbbb95cedf
Dharma Yuartha <71548487+Dhyuartha@users.noreply.github.com>
2024-11-16 02:56:35 +0000

    updated

--- ardToRaspSerial/RaspToArdSerial.py
+++ ardToRaspSerial/RaspToArdSerial.py
@@ -12,36 +12,37 @@ def readCmdResponse():
 def printCmdResponse(lineData):
 	for lines in lineData:
 		print(lines.decode('utf-8').strip())
+		
+def invalidCheck(byteCmdStr):
+	time.sleep(2)
+	serResponse = readCmdResponse()
+	for lines in serResponse:
+		temp = lines.decode('utf-8').strip()
+		if "invalid" in temp:
+			print('invalid command recieved, resending byteCmdStr')
+			time.sleep(0.5)
+			ser.write(byteCmdStr)
+			serResponse = readCmdResponse()
+	printCmdResponse(serResponse)
     
 def status():
     ser.write(b'status\n')
     response = readCmdResponse()
     printCmdResponse(response)
     #print("This is the last line of the response to status" + response[-1].decode('utf-8').strip())
-    
+	
 def setMotor(param1, param2):
     cmdStr = "setMotor " + str(param1) + ", " + str(param2)
     byteCmdStr = cmdStr.encode('utf-8')
     ser.write(byteCmdStr)
-    printCmdResponse(readCmdResponse())
-    time.sleep(1.5)
+    invalidCheck(byteCmdStr)
+
     
 def reset():
 	ser.write(b'reset\n')
-	printCmdResponse(readCmdResponse())
-	time.sleep(1.5)
-
-def manualOn():
-    ser.write(b'manual on\n')
-    printCmdResponse(readCmdResponse())
-    time.sleep(1.5)
-
-def manualOff():
-	ser.write(b'manual off\n')
-	response = readCmdResponse()
-	print(response.length())
-	printCmdResponse(response)
-	time.sleep(1.5)
+	serResponse = readCmdResponse()
+	invalidCheck(b'reset\n')
+	time.sleep(2.5)
     
 def readBoot():
     print("waiting for start up\n")
@@ -56,36 +57,36 @@ def rotate(param1, param2):
     cmdStr = "rotate " + str(param1) + ", " + str(param2)
     byteCmdStr = cmdStr.encode('utf-8')
     ser.write(byteCmdStr)
-    printCmdResponse(readCmdResponse())
-    time.sleep(1.5)
+    invalidCheck(byteCmdStr)
+    time.sleep(2.5)
     
 def clampOn():
 	cmdStr = "clamp on"
 	byteCmdStr = cmdStr.encode('utf-8')
 	ser.write(byteCmdStr)
-	printCmdResponse(readCmdResponse())
-	time.sleep(1.5)
+	invalidCheck(byteCmdStr)
+	time.sleep(2.5)
     
 def clampOff():
 	cmdStr = "clamp off"
 	byteCmdStr = cmdStr.encode('utf-8')
 	ser.write(byteCmdStr)
-	printCmdResponse(readCmdResponse())
-	time.sleep(1.5)
+	invalidCheck(byteCmdStr)
+	time.sleep(2.5)
 
 def suckerOn():
 	cmdStr = "sucker on"
 	byteCmdStr = cmdStr.encode('utf-8')
 	ser.write(byteCmdStr)
-	printCmdResponse(readCmdResponse())
-	time.sleep(1.5)
+	invalidCheck(byteCmdStr)
+	time.sleep(2.5)
 
 def suckerOff():
 	cmdStr = "sucker off"
 	byteCmdStr = cmdStr.encode('utf-8')
 	ser.write(byteCmdStr)
-	printCmdResponse(readCmdResponse())
-	time.sleep(1.5)
+	invalidCheck(byteCmdStr)
+	time.sleep(2.5)
 
 readBoot()
 
@@ -97,9 +98,10 @@ setMotor(3, 160)
 
 setMotor(1, 180)
 
-setMotor(3, 120)
+setMotor(3, 110)
 
 #suction on
+suckerOn();
 
 setMotor(3, 160)
 
@@ -108,6 +110,7 @@ setMotor(1, 140)
 rotate(380, -1)
 
 #drop plate
+suckerOff();
 
 rotate(100, 1)
 
